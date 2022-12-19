239797ed87a5808682bce7ad7258db8424e8fb4a
Dlochinbek <dehkonov9@gmail.com>
2022-12-19 15:01:25 +0500

    first commit

---
+++ app/app.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const loader = document.querySelector('.loader')
+
+    setTimeout(() => {
+     loader.style.opacity = '2'
+     setTimeout(() => {
+        loader.style.display = 'none'
+     }, timeout = .5000);   
+    
+    }, timeout = 2000)
+})
